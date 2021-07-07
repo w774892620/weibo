3ae87e113821b7aacb54c23c3e239effe577c887
jc <774892620@qq.com>
2021-07-07 11:44:21 +0800

    关注用户动态流

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -14,7 +14,7 @@ class StaticPagesController extends Controller
         if (Auth::check()) {
             $feed_items = Auth::user()->feed()->paginate(30);
         }
-
+        
         return view('static_pages/home', compact('feed_items'));
     }
 
